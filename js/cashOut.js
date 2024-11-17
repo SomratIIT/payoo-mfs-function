f87e51fd7f0b8861946ea74d7013e40414178a04
SomratIIT <101197949+SomratIIT@users.noreply.github.com>
2024-11-17 15:53:17 +0600

    Latest Work with function about Cash Out

---
+++ js/cashOut.js
@@ -0,0 +1,22 @@
+document.getElementById('btn-cash-out').addEventListener('click',function(event){
+    event.preventDefault();
+
+
+
+
+
+    const cashOut = getInputFieldValueById('input-cash-out');
+    const pinNumber = getInputFieldValueById('input-cash-out-pin');
+
+    if(pinNumber===1){
+
+        const balance =getTextValueById('account-balance');
+        const newBalance = balance - cashOut ;
+
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+
+    else{
+        alert('Get Out');
+    }
+})
